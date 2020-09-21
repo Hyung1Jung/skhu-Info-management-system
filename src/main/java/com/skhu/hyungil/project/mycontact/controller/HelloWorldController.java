9816342be1223bf332c4de933cca4a607b469878
Hyung1Jung <junghyungile@gmail.com>
2020-09-21 19:59:37 +0900

    api test

--- src/main/java/com/skhu/hyungil/project/mycontact/controller/HelloWorldController.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/controller/HelloWorldController.java
@@ -2,13 +2,14 @@ package com.skhu.hyungil.project.mycontact.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
     @GetMapping(value = "/api/helloWorld")
-    public String helloWorld() {
-      return "helloWorld";
+    public static String helloWorld() {
+      return "HelloWorld";
     }
+
 }
