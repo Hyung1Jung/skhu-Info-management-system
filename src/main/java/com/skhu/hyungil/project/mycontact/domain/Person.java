0b42cc24f4477591953be72b8ba9a6e7d1360cb5
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-05 07:19:38 +0900

    Entity 추가

--- src/main/java/com/skhu/hyungil/project/mycontact/domain/Person.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/domain/Person.java
@@ -15,4 +15,37 @@ public class Person {
 
     private int age;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // 결과가 해쉬 값으로 나오는 것을 방지하기 위해 toString, sysout을 사용하면 해쉬 값이 나옴.
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
