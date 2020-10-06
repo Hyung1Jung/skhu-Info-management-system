c3397321f644bca209db6d184d0da751e8239d50
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-06 17:07:04 +0900

    JPA 상속

---
+++ src/main/java/com/skhu/hyungil/project/mycontact/repository/BlockRepository.java
@@ -0,0 +1,7 @@
+package com.skhu.hyungil.project.mycontact.repository;
+
+import com.skhu.hyungil.project.mycontact.domain.Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<Block, Long> {
+}
