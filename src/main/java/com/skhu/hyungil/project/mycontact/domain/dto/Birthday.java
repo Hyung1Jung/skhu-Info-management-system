b6ce936c3d57f182cd69de2f0ec2e5f4ead480a2
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-12 00:52:58 +0900

    @Embedddble 추가

--- src/main/java/com/skhu/hyungil/project/mycontact/domain/dto/Birthday.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/domain/dto/Birthday.java
@@ -1,8 +1,16 @@
 package com.skhu.hyungil.project.mycontact.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Embeddable;
 
-@Embeddable // 엔티티에 속해있는 Dtd
+@Embeddable // 엔티티에 속해있는 Dto
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // 해쉬 값을 toString으로
 public class Birthday {
     private int yearOfBirthday;
     private int monthOfBirthday;
