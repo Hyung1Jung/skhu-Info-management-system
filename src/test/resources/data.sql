b01e5de832aba995f696a2ea7b6d5c7118b51007
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-12 06:20:15 +0900

    data 추가

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (1, 'hyungil', 27, 'A', 1994, 10, 10);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (2, 'yunggon', 27, 'B', 1994, 4, 1);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'gihyug', 26, 'A', 1995, 3, 2);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'hosuck', 26, 'AB', 1995, 2, 3);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'jinmin', 26, 'O', 1995, 10, 4);
