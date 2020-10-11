3c8ba2b782a3b3ca0bec906d9c8031d38369316a
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-12 06:20:55 +0900

    Test code

--- src/main/java/com/skhu/hyungil/project/mycontact/repository/PersonRepository.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/repository/PersonRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
@@ -15,7 +14,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByBloodType(String bloodType);
 
-    @Query(value = "select person from where month_of_birthday = :monthOfBirthday and day_of_birthday = :dayOfBirthday", nativeQuery = true) // ?1 -> 첫번째 인자자 //파라미터 맵핑이 더 효율적
-    List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday, @Param("dayOfBirthday") int dayOfBirthday);
+    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday") // ?1 -> 첫번째 인자자 //파라미터 맵핑이 더 효율적
+    List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
 
 }
--- src/test/java/com/skhu/hyungil/project/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/skhu/hyungil/project/mycontact/repository/PersonRepositoryTest.java
@@ -1,19 +1,13 @@
 package com.skhu.hyungil.project.mycontact.repository;
 
 import com.skhu.hyungil.project.mycontact.domain.Person;
-import com.skhu.hyungil.project.mycontact.domain.dto.Birthday;
-import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -24,75 +18,36 @@ class PersonRepositoryTest {
     void crud() {
         Person person = new Person();
 
-        person.setName("형일");
+        person.setName("hosuck");
         person.setAge(27);
         person.setBloodType("A");
 
         personRepository.save(person);
 
-        System.out.println(personRepository.findAll());
+        List<Person> result = personRepository.findByName("hosuck");
 
-        List<Person> people = personRepository.findAll();
-
-        assertThat(people.size()).isEqualTo(1);
-        assertThat(people.get(0).getName()).isEqualTo("형일");
-        assertThat(people.get(0).getAge()).isEqualTo(27);
-        assertThat(people.get(0).getBloodType()).isEqualTo("A");
-    }
-
-    @Test
-    void hashCodeEquals() {
-        Person person1 = new Person("형일", 27, "A");
-        Person person2 = new Person("형일", 27, "A");
-
-        System.out.println(person1.equals(person2));
-        System.out.println(person1.hashCode());
-        System.out.println(person2.hashCode());
-
-        Map<Person, Integer> map = new HashMap<>();
-        map.put(person1, person1.getAge());
-
-        System.out.println(map);
-        System.out.println(map.get(person2));
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getName()).isEqualTo("hosuck");
+        assertThat(result.get(0).getAge()).isEqualTo(27);
+        assertThat(result.get(0).getBloodType()).isEqualTo("A");
     }
 
     @Test
     void findByBloodType() {
-        givenPerson("형일", 27, "A");
-        givenPerson("영곤", 27, "B");
-        givenPerson("기혁", 26, "o");
-        givenPerson("일권", 26, "AB");
-        givenPerson("호석", 25, "A");
-        givenPerson("준성", 25, "A");
 
         List<Person> result = personRepository.findByBloodType("A");
 
-        result.forEach((System.out::println));
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getName()).isEqualTo("hyungil");
+        assertThat(result.get(1).getName()).isEqualTo("gihyug");
     }
 
     @Test
     void findByBirthdayBetween() {
-        givenPerson("형일", 27, "A", LocalDate.of(1994, 10, 10));
-        givenPerson("영곤", 27, "B", LocalDate.of(1994, 1, 1));
-        givenPerson("기혁", 26, "o", LocalDate.of(1995, 2, 2));
-        givenPerson("일권", 26, "AB", LocalDate.of(1995, 3, 3));
-        givenPerson("호석", 25, "A", LocalDate.of(1998, 10, 30));
+        List<Person> result = personRepository.findByMonthOfBirthday(10);
 
-        List<Person> result = personRepository.findByMonthOfBirthday(10, 30);
-
-        result.forEach(System.out::println);
-    }
-
-    // findByBloodType()에 생일 값이 없기 때문에 메소드 오버로딩 해준다.
-    private void givenPerson(String name, int age, String bloodType) {
-        givenPerson(name, age, bloodType, null);
-    }
-
-    private void givenPerson(String name, int age, String bloodType, LocalDate birthday) {
-        Person person = new Person(name, age, bloodType);
-        person.setBirthday(new Birthday(birthday.getYear(), birthday.getMonthValue(), birthday.getDayOfMonth()));
-
-        personRepository.save(person);
-
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getName()).isEqualTo("hyungil");
+        assertThat(result.get(1).getName()).isEqualTo("jinmin");
     }
 }
