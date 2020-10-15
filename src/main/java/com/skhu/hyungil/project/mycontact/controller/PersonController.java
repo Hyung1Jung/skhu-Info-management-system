6c4431a1cc409a019b0e7fd4e1fe0d4b9eec8dc5
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-15 16:56:40 +0900

    deleteMapping 추가

--- src/main/java/com/skhu/hyungil/project/mycontact/controller/PersonController.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/controller/PersonController.java
@@ -44,6 +44,16 @@ public class PersonController {
         personService.modify(id, name);
 
         log.info("person -> {} ", personRepository.findAll());
-
     }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personService.delete(id);
+
+        log.info("person -> {} ", personRepository.findAll());
+    }
+
+
+
+
 }
