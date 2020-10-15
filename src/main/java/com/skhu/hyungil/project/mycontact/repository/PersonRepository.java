72b0c65c264cfce7dd56722bcf2716705a22cbc0
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-15 16:49:56 +0900

    deleted - nativeQuery 추가

--- src/main/java/com/skhu/hyungil/project/mycontact/repository/PersonRepository.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/repository/PersonRepository.java
@@ -14,7 +14,11 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByBloodType(String bloodType);
 
-    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday") // ?1 -> 첫번째 인자자 //파라미터 맵핑이 더 효율적
+    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")
+        // ?1 -> 첫번째 인자자 //파라미터 맵핑이 더 효율적
     List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
+
+    @Query(value = "select * from Person person where person.deleted = true", nativeQuery = true) //nativeQuery를 통해서 삭제된 정보까지 확인
+    List<Person> findPeopleDeleted();
 
 }
