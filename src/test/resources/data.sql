aba6346626a188eea8929fd5acaf8b0ec8c76fc8
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-16 17:21:49 +0900

    data 수정

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,8 +1,8 @@
-insert into person(`id`, `name`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`) values (1, 'hyungil', 'A', 1994, 10, 10, 'programmer');
-insert into person(`id`, `name`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (2, 'yunggon', 'B', 1994, 4, 1);
-insert into person(`id`, `name`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'gihyug', 'A', 1995, 3, 2);
-insert into person(`id`, `name`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'hosuck', 'AB', 1995, 2, 3);
-insert into person(`id`, `name`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'jinmin', 'O', 1995, 10, 4);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`) values (1, 'hyungil', 1994, 10, 10, 'programmer');
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (2, 'yunggon', 1994, 4, 1);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'gihyug', 1995, 3, 2);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'hosuck', 1995, 2, 3);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'jinmin', 1995, 10, 4);
 
 insert into block(`id`, `name`) values (1, 'gihyug');
 insert into block(`id`, `name`) values (2, 'hosuck');
