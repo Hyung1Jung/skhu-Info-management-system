f2259f8b3032e754eecc5586ae965d7fcdaea51f
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-19 15:43:44 +0900

    getPerosn TestCode

--- src/test/java/com/skhu/hyungil/project/mycontact/controller/PersonControllerTest.java
+++ src/test/java/com/skhu/hyungil/project/mycontact/controller/PersonControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -21,6 +22,7 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,15 +36,16 @@ class PersonControllerTest {
     private PersonController personController;
     @Autowired
     private ObjectMapper objectMapper;
-
     @Autowired
     PersonRepository personRepository;
+    @Autowired
+    private MappingJackson2HttpMessageConverter messageConverter;
 
     private MockMvc mockMvc;
 
     @BeforeEach // 해당 메소드는 매 test마다 먼저 실행이됨
     void beforeEach() {
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(personController).setMessageConverters(messageConverter).build();
     }
 
     @Test
@@ -51,8 +54,17 @@ class PersonControllerTest {
                 MockMvcRequestBuilders.get("/api/person/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("hyungil"));
+                .andExpect(jsonPath("$.name").value("hyungil")) // $ 객체의미 // value 가져온 값에 대한 검증
+                .andExpect(jsonPath("hobby").isEmpty())
+                .andExpect(jsonPath("address").isEmpty())
+                .andExpect(jsonPath("$.birthday").value("1994-10-10"))
+                .andExpect(jsonPath("$.job").isEmpty())
+                .andExpect(jsonPath("$.phoneNumber").isEmpty())
+                .andExpect(jsonPath("$.deleted").value(false))
+                .andExpect(jsonPath("$.age").isNumber()) // 숫자의 값이 value를 가지고 있는지
+                .andExpect(jsonPath("$.birthdayToday").isBoolean());
     }
+
 
     @Test
     void postPerson() throws Exception {
@@ -71,7 +83,7 @@ class PersonControllerTest {
 
     @Test
     void modifyPerson() throws Exception {
-        PersonDto dto = PersonDto.of("hyungil","programming","안양", LocalDate.now(), "programmer","010-1234-1234");
+        PersonDto dto = PersonDto.of("hyungil", "programming", "안양", LocalDate.now(), "programmer", "010-1234-1234");
 
         mockMvc.perform(
                 MockMvcRequestBuilders.put("/api/person/1")
@@ -82,18 +94,20 @@ class PersonControllerTest {
 
         Person result = personRepository.findById(1L).get();
 
-        assertThat(result.getName()).isEqualTo("hyungil");
-        assertThat(result.getHobby()).isEqualTo("programming");
-        assertThat(result.getAddress()).isEqualTo("안양");
-        assertThat(result.getBirthday()).isEqualTo(Birthday.of(LocalDate.now()));
-        assertThat(result.getJob()).isEqualTo("programmer");
-        assertThat(result.getPhoneNumber()).isEqualTo("010-1234-1234");
-
+        // 한 번의 검증으로 6개의 잘못된 것들 보기
+        assertAll(
+                () -> assertThat(result.getName()).isEqualTo("hyungil"),
+                () -> assertThat(result.getHobby()).isEqualTo("programming"),
+                () -> assertThat(result.getAddress()).isEqualTo("안양"),
+                () -> assertThat(result.getBirthday()).isEqualTo(Birthday.of(LocalDate.now())),
+                () -> assertThat(result.getJob()).isEqualTo("programmer"),
+                () -> assertThat(result.getPhoneNumber()).isEqualTo("010-1234-1234")
+        );
     }
 
     @Test
     void modifyPersonPersonIfNameIsDifferent() throws Exception {
-        PersonDto dto = PersonDto.of("minsub","programming","안양", LocalDate.now(), "programmer","010-1234-1234");
+        PersonDto dto = PersonDto.of("minsub", "programming", "안양", LocalDate.now(), "programmer", "010-1234-1234");
 
         assertThrows(NestedServletException.class, () ->
                 mockMvc.perform(
@@ -114,7 +128,6 @@ class PersonControllerTest {
                 .andExpect(status().isOk());
 
         assertThat(personRepository.findById(1L).get().getName()).isEqualTo("hyungilModifed");
-
 
 
     }
