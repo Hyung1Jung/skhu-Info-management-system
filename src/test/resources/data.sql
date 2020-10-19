52ed6138a3f902a6f2e64aa9bd072ba943e61ee0
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-19 17:51:57 +0900

    data 수정

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,4 +3,6 @@ insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_o
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'gihyug', 1995, 3, 2);
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'hosuck', 1995, 2, 3);
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'jinmin', 1995, 10, 4);
-
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`, `hobby`, `phone_number`, `address`)
+    values(6, 'jiwon', 1994, 7, 10, 'student', 'reading', '010-1111-2222', 'seoul');
+insert into person(`id`, `name`, `deleted`) values(7, 'jiyun', true);
