45bccb4f2759d5165238256b514091b645cc4b9e
Hyung1Jung <HyungilJung94@gmail.com>
2020-10-26 00:25:36 +0900

    리팩토링

--- src/main/java/com/skhu/hyungil/project/mycontact/exception/dto/ErrorResponse.java
+++ src/main/java/com/skhu/hyungil/project/mycontact/exception/dto/ErrorResponse.java
@@ -1,12 +1,18 @@
 package com.skhu.hyungil.project.mycontact.exception.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorResponse {
     private int code;
     private String message;
+
+    public static ErrorResponse of(HttpStatus httpStatus, String message) {
+        return new ErrorResponse(httpStatus.value(), message);
+    }
 }
 
